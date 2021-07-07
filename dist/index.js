e3a4edddbfef21a809960a45a190dc684786bdc7
iuslkae <iuslkae@protonmail.com>
2021-07-07 15:53:44 -0500

    update options: error handling

--- dist/index.js
+++ dist/index.js
@@ -2,8 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const yargs = require("yargs");
 const child_process_1 = require("child_process");
+const util_1 = require("util");
 const fs_1 = require("fs");
 const path_1 = require("path");
+const asyncExec = util_1.promisify(child_process_1.exec);
 const exists = (path) => {
     try {
         fs_1.accessSync(path);
@@ -24,34 +26,30 @@ const command = `yarn add -D eslint eslint-config-prettier eslint-plugin-prettie
 const tCommand = `yarn add -D typescript ttypescript ts-node nodemon ts-transformer-keys @types/node -E`;
 const genFile = (file) => {
     if (!exists(path_1.join(process.cwd(), file))) {
-        fs_1.copyFileSync(require.resolve(`iusllint/lintfiles/${file}`), path_1.join(process.cwd(), file));
+        try {
+            fs_1.copyFileSync(require.resolve(`iusllint/lintfiles/${file}`), path_1.join(process.cwd(), file));
+        }
+        catch (e) {
+            console.log(e);
+        }
     }
 };
 const argv = yargs.command('$0', 'the default command', (yargs) => {
-    return yargs
-        .option('t', {
-        alias: 'typescript',
-        describe: 'generate aditional standard tsconfig.json',
-        type: 'boolean',
-    })
-        .option('ts', {
-        alias: 'tsdev',
-        describe: 'generate aditional standard tsconfig.json and installs basic typescript dev env',
-        type: 'boolean',
+    return yargs.options({
+        t: {
+            alias: 'typescript',
+            describe: 'generate aditional standard tsconfig.json',
+            type: 'boolean',
+        },
+        ts: {
+            alias: 'tsdev',
+            describe: 'generate aditional standard tsconfig.json and installs basic typescript dev env',
+            type: 'boolean',
+        },
     });
-}, (argv) => {
-    child_process_1.exec(command, (error, stdout, stderr) => {
-        if (error) {
-            console.log(`error: ${error.message}`);
-            return;
-        }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
-        }
-        console.log(`stdout: ${stdout}`);
-    });
+}, async (argv) => {
     console.log(command);
+    await asyncExec(command).then(console.log).catch(console.log);
     genFile(LintFiles.eslintrc);
     genFile(LintFiles.prettierrc);
     if (argv.t) {
@@ -59,17 +57,8 @@ const argv = yargs.command('$0', 'the default command', (yargs) => {
     }
     if (argv.ts) {
         genFile(LintFiles.tsconfig);
-        child_process_1.exec(tCommand, (error, stdout, stderr) => {
-            if (error) {
-                console.log(`error: ${error.message}`);
-                return;
-            }
-            if (stderr) {
-                console.log(`stderr: ${stderr}`);
-                return;
-            }
-            console.log(`stdout: ${stdout}`);
-        });
+        console.log(tCommand);
+        await asyncExec(tCommand).then(console.log).catch(console.log);
     }
 }).argv;
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import * as yargs from 'yargs';
 import { exec } from 'child_process';
+import { promisify } from 'util';
 import { copyFileSync, accessSync, PathLike } from 'fs';
 import { join } from 'path';
+const asyncExec = promisify(exec);
 const exists = (path: PathLike): boolean => {
   try {
     accessSync(path);
@@ -21,10 +23,14 @@ const tCommand = `yarn add -D typescript ttypescript ts-node nodemon ts-transfor
 
 const genFile = (file: LintFiles): void => {
   if (!exists(join(process.cwd(), file))) {
-    copyFileSync(
-      require.resolve(`iusllint/lintfiles/${file}`),
-      join(process.cwd(), file),
-    );
+    try {
+      copyFileSync(
+        require.resolve(`iusllint/lintfiles/${file}`),
+        join(process.cwd(), file),
+      );
+    } catch (e) {
+      console.log(e);
+    }
   }
 };
 
@@ -32,33 +38,23 @@ const argv = yargs.command(
   '$0',
   'the default command',
   (yargs) => {
-    return yargs
-      .option('t', {
+    return yargs.options({
+      t: {
         alias: 'typescript',
         describe: 'generate aditional standard tsconfig.json',
         type: 'boolean',
-      })
-      .option('ts', {
+      },
+      ts: {
         alias: 'tsdev',
         describe:
           'generate aditional standard tsconfig.json and installs basic typescript dev env',
         type: 'boolean',
-      });
-    // .option();
-  },
-  (argv) => {
-    exec(command, (error, stdout, stderr) => {
-      if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-      }
-      if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-      }
-      console.log(`stdout: ${stdout}`);
+      },
     });
+  },
+  async (argv) => {
     console.log(command);
+    await asyncExec(command).then(console.log).catch(console.log);
     genFile(LintFiles.eslintrc);
     genFile(LintFiles.prettierrc);
     if (argv.t) {
@@ -66,17 +62,8 @@ const argv = yargs.command(
     }
     if (argv.ts) {
       genFile(LintFiles.tsconfig);
-      exec(tCommand, (error, stdout, stderr) => {
-        if (error) {
-          console.log(`error: ${error.message}`);
-          return;
-        }
-        if (stderr) {
-          console.log(`stderr: ${stderr}`);
-          return;
-        }
-        console.log(`stdout: ${stdout}`);
-      });
+      console.log(tCommand);
+      await asyncExec(tCommand).then(console.log).catch(console.log);
     }
   },
 ).argv;
