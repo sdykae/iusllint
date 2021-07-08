b9642e7e12de6a013e201d5fb250dc732b968e69
iuslkae <iuslkae@protonmail.com>
2021-07-08 16:09:15 -0500

    update tsconfig

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const yargs = require("yargs");
+const tslib_1 = require("tslib");
+const yargs = tslib_1.__importStar(require("yargs"));
 const child_process_1 = require("child_process");
 const util_1 = require("util");
 const fs_1 = require("fs");
